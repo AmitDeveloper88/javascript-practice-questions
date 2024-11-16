6c9461dcb17ac78a0ef604bca113a9982606bb3e
AmitDeveloper88 <amitdeveloper96@gmail.com>
2024-11-16 18:20:18 +0530

    Added remove duplicates function

---
+++ javascript-practice-questions/arrays/remove-duplicates.js
@@ -0,0 +1,5 @@
+// Example: arrays/remove-duplicates.js
+function removeDuplicates(arr) {
+  return arr.filter((item, index) => arr.indexOf(item) === index);
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
